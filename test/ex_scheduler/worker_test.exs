7e19297761e4a1c2344eb5b0f73a4ddc40b8dbfb
Espartaco Palma <esparta@gmail.com>
2020-04-22 13:07:50 -0700

    Testing Genserver implementation
    Using :ets as the global state management

--- test/ex_scheduler/worker_test.exs
+++ test/ex_scheduler/worker_test.exs
@@ -3,32 +3,59 @@ defmodule ExScheduler.WorkerTest do
   alias ExScheduler.Worker
 
   defmodule Hello do
-    def perform(), do: IO.puts("Perform!")
-    def hello(), do: IO.puts("Hello!")
-    def hello(name), do: IO.puts("Hello #{name}!")
+    def perform() do
+      :ets.insert(:table, {:foo, :bar})
+    end
+
+    def hello() do
+      :ets.insert(:table, {:foo, :bar})
+    end
+
+    def hello(_name) do
+      :ets.insert(:table, {:foo, :bar})
+    end
+  end
+
+  def setup_server(config, waiting \\ 1010) do
+    GenServer.start_link(Worker, config)
+    Process.sleep(waiting)
+  end
+
+  setup _ do
+    :ets.new(:table, [:named_table, :duplicate_bag, :public])
+    :ok
   end
 
   test "use perform/0 function by default" do
     config = [%{cron: "* * * * * *", module: Hello}]
-    GenServer.start_link(Worker, config)
-    Process.sleep(1050)
+    setup_server(config)
+    assert Enum.count(:ets.lookup(:table, :foo)) > 0
   end
 
-  @tag :skip
   test "use function with 0 arity by default" do
     config = [%{cron: "* * * * * *", module: Hello, function: :hello}]
+
+    setup_server(config)
+    assert Enum.count(:ets.lookup(:table, :foo)) > 0
   end
 
-  @tag :skip
   test "use custom function with args" do
     config = [%{cron: "* * * * * *", module: Hello, function: :hello, args: ["world"]}]
+
+    setup_server(config)
+    assert Enum.count(:ets.lookup(:table, :foo)) > 0
   end
 
-  @tag :skip
   test "run every second for 5 times" do
+    config = [%{cron: "* * * * * *", module: Hello}]
+
+    setup_server(config, 5050)
+    assert Enum.count(:ets.lookup(:table, :foo)) == 5
   end
 
-  @tag :skip
   test "run every 2 seconds" do
+    config = [%{cron: "*/2 * * * * *", module: Hello}]
+    setup_server(config, 4050)
+    assert Enum.count(:ets.lookup(:table, :foo)) == 2
   end
 end
